fbe86d04bdb20470a9a8a18029f8cb47e6cf4c1a
Tanveer-hub570 <tanveermustafa57@gmail.com>
2025-04-11 01:15:11 +0500

    App.js

--- Quiz1/App.js
+++ Quiz1/App.js
@@ -56,6 +56,6 @@ const styles = StyleSheet.create({
   weatherIcon: { width: 100, height: 100, marginVertical: 10 },
   temperature: { fontSize: 40, fontWeight: 'bold' },
   description: { fontSize: 20 },
-});
+}); 
 
 export default App;
